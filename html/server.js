c8b1fa9ec57ecac464b1a2e84054f093bcfe3434
hefang <he529564582@163.com>
2018-09-30 22:03:33 +0800

    new

--- html/server.js
+++ html/server.js
@@ -1,25 +1,49 @@
+// var http = require('http')
+// var fs = require('fs')
+// var path = require('path')
+// var url = require('url')
+
+// http.createServer(function(req, res){
+
+//   var pathObj = url.parse(req.url, true)
+
+//   switch (pathObj.pathname) {
+//     case '/getWeather':
+//       res.end(JSON.stringify({beijing: 'sunny'}))
+//       break;
+
+//     default:
+//       fs.readFile(path.join(__dirname, pathObj.pathname), function(e, data){
+//         if(e){
+//           res.writeHead(404, 'not found')
+//           res.end('<h1>404 Not Found</h1>')
+//         }else{
+//           res.end(data)
+//         }
+//       }) 
+//   }
+// }).listen(8080)
+
+
 var http = require('http')
 var fs = require('fs')
 var path = require('path')
 var url = require('url')
 
-http.createServer(function(req, res){
-
-  var pathObj = url.parse(req.url, true)
-
-  switch (pathObj.pathname) {
-    case '/getWeather':
-      res.end(JSON.stringify({beijing: 'sunny'}))
-      break;
-
-    default:
-      fs.readFile(path.join(__dirname, pathObj.pathname), function(e, data){
-        if(e){
-          res.writeHead(404, 'not found')
-          res.end('<h1>404 Not Found</h1>')
-        }else{
-          res.end(data)
-        }
-      }) 
-  }
+http.createServer(function(req,res){
+    var pathObj = url.parse(req.url,true)
+    switch(pathObj.pathname){
+        case '/getWeather':
+        res.end(JSON.stringify({beijing:'sunny'}))
+        break;
+        default:
+        fs.readFile(path.join(__dirname,pathObj,pathname),function(e,data){
+            if(e){
+                res.writeHead(404,'not found')
+                res.end('<h1>404 not found</h1>')
+            }else{
+                res.end(data)
+            }
+        })
+    }
 }).listen(8080)
