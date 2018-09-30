b0178ea0b22bcadabd17c56730c8b3c6cc824e2e
hefang <he529564582@163.com>
2018-09-30 18:20:59 +0800

    add

--- html/server.js
+++ html/server.js
@@ -3,20 +3,23 @@ var fs = require('fs')
 var path = require('path')
 var url = require('url')
 
-http.createServer(function(res,req){
-    var pathObj = url.parse(req.url,true)
-    switch (pathObj.pathname){
-        case '/getWeather' :
-        res.setEncoding(JSON.stringify({biejing:'sunny'}))
-        break;
-        default:
-        fs.readFile(parse.join(__dirname,pathObj.pathname),function(e,data){
-            if(e){
-                res.writehead(404,'not found')
-                res.end('<h1>404 not found</h1>')
-            }else{
-                res.end(data)
-            }
-        })
-    }
+http.createServer(function(req, res){
+
+  var pathObj = url.parse(req.url, true)
+
+  switch (pathObj.pathname) {
+    case '/getWeather':
+      res.end(JSON.stringify({beijing: 'sunny'}))
+      break;
+
+    default:
+      fs.readFile(path.join(__dirname, pathObj.pathname), function(e, data){
+        if(e){
+          res.writeHead(404, 'not found')
+          res.end('<h1>404 Not Found</h1>')
+        }else{
+          res.end(data)
+        }
+      }) 
+  }
 }).listen(8080)
