c1a92d5e6ecf4e5a24a750deb22ba48640070825
hefang <he529564582@163.com>
2018-10-02 17:47:41 +0800

    1

--- html/server.js
+++ html/server.js
@@ -25,11 +25,34 @@
 // }).listen(8080)
 
 
+// var http = require('http')
+// var fs = require('fs')
+// var path = require('path')
+// var url = require('url')
+
+// http.createServer(function(req,res){
+//     var pathObj = url.parse(req.url,true)
+//     switch(pathObj.pathname){
+//         case '/getWeather':
+//         res.end(JSON.stringify({beijing:'sunny'}))
+//         break;
+//         default:
+//         fs.readFile(path.join(__dirname,pathObj.pathname),function(e,data){
+//             if(e){
+//                 res.writeHead(404,'not found')
+//                 res.end('<h1>404 not found</h1>')
+//             }else{
+//                 res.end(data)
+//             }
+//         })
+//     }
+// }).listen(8080)
+
+
 var http = require('http')
 var fs = require('fs')
 var path = require('path')
 var url = require('url')
-
 http.createServer(function(req,res){
     var pathObj = url.parse(req.url,true)
     switch(pathObj.pathname){
@@ -37,9 +60,9 @@ http.createServer(function(req,res){
         res.end(JSON.stringify({beijing:'sunny'}))
         break;
         default:
-        fs.readFile(path.join(__dirname,pathObj.pathname),function(e,data){
+        fs.readFile(parse.join(__dirname,pathObj.pathname),function(e,data){
             if(e){
-                res.writeHead(404,'not found')
+                res.writeHead(404, 'not found')
                 res.end('<h1>404 not found</h1>')
             }else{
                 res.end(data)
