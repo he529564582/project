b3784270f82c63797e91c4dbacb3ae3865d520d3
hefang <he529564582@163.com>
2018-12-11 22:37:53 +0800

    add

---
+++ js/1211.js
@@ -0,0 +1,8 @@
+function toReverseString(str){
+    return str.split("").reverse().join("")
+}
+console.log(toReverseString("abcd"))
+// str.split()
+将字符串对象分割成字符串数组
+即:(3) ["a", "b", "c","d"]
+length: 3
