4179d7cc43ae9b72f381f92a6d0e756216c3114d
hefang <he529564582@163.com>
2019-05-18 15:56:05 +0800

    fix

--- js/0518.js
+++ js/0518.js
@@ -3,3 +3,13 @@ function a(){
     console.log(i)
   }
 }
+
+class Person {
+  constructor(name,age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHello(){
+    console.log(`hello,${this.name},i am ${this.age} year old`);
+  }
+}
